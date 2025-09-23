b0ebfd35d64d6d471f6b966e8b8d69033d34d4b2
Edsouzaa <edsonhenriqueabreu@gmail.com>
2025-09-22 22:37:47 -0300

    New: Prototipo do App.js, TodoItem e toDoList

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'; 
+import ToDoList from "./toDoList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App(){ 
+  // variavel de estado que armazenará todas as atividades 
+  const [todos, setTodos] = useState([]); // [] indica um array
+  
+  // variavel de estado relativo a tarefa 
+  const [newTask, setNewTask] = useState(''); 
+  
+  // variavel de estado relativa a data 
+  const [dataDia, setDataDia] = useState(''); 
+  
+  // variavel de estado relativo a hora 
+  const [hora, setHora] = useState(''); 
+  
+  // Funcao adiciona uma tarefa na lista 
+  const addTask = () => { 
+    if(newTask.trim() === '') return; 
+    
+    // Crio um objeto que representa a atividade 
+    const newToDo = { 
+      id: Date.now(), // Crio um id unico baseado na hora atual 
+      text: newTask, 
+      data: dataDia, 
+      hora: hora 
+    }; 
+    
+    setTodos([...todos, newToDo]); 
+    
+    // Limpar as variaveis de estado 
+    setNewTask(''); 
+    setDataDia(''); 
+    setHora(''); 
+  }; 
+  
+  // Função que remove as tarefas pelo id 
+  const removeTask = (id) => 
+    setTodos(todos.filter((todo) => todo.id !== id)) 
+  ; 
 
-export default App;
+  return( 
+    <div> 
+      <h1>Minha To-do-List</h1> 
+      <div> 
+        <input 
+          type='text' 
+          placeholder='Digite uma nova tarefa' 
+          value={newTask} 
+          onChange={(e) => setNewTask(e.target.value)} 
+        /> 
+        <button onClick={addTask}>Adicionar</button> 
+      </div> 
+      <div> 
+        <ToDoList todos={todos} removeTask={removeTask}/> 
+      </div> 
+    </div> 
+  ); 
+};
